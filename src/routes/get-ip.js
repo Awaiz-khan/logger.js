23bd941be4c5cecfb5f6c7bab4cb2656a1389074
Awaiz-khan <Mohammed.Awaiz@piloggroup.com>
2023-06-09 17:06:54 +0530

    new

--- src/routes/get-ip.js
+++ src/routes/get-ip.js
@@ -1,11 +1,18 @@
 
-import os from 'os';
+import axios from "axios";
 
 export const basicRoutes = {
     path: '/api/get-ip',
     method: 'get',
     handler: async (req, res) => {
-        const ipAddress = os.networkInterfaces();
-    res.send(ipAddress);
+        try {
+            const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+            const response = await axios.get(`https://geolocation-api.com/ip/${ipAddress}`);
+            const { country, region, city } = response.data;
+        
+            res.send(`Your location: ${city}, ${region}, ${country}`);
+          } catch (error) {
+            res.status(500).send('Error retrieving location');
+          }
     }
 }
