a9a900fbc57e6016a60174fc228d0a785502e7e3
Awaiz-khan <135847064+Awaiz-khan@users.noreply.github.com>
2023-06-15 14:29:45 +0000

    updated chaching and cookie storage method

--- src/server.js
+++ src/server.js
@@ -1,78 +1,66 @@
-import express from "express"
-import os from "os"
-import 'dotenv/config'
-import requestIp from "request-ip";
-import ip from "ip";
-import useragent from "useragent";
-import device from 'express-device';
- import geoip  from 'geoip-lite';
-import axios from 'axios';
 
+import express from 'express';
+import os from 'os';
+import 'dotenv/config';
+import requestIp from 'request-ip';
+import ip from 'ip';
+import useragent from 'useragent';
+import device from 'express-device';
+import geoip from 'geoip-lite';
+import axios from 'axios';
+import cookieParser from 'cookie-parser';
 
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import { routes } from "./allRoutes.js";
+import { routes } from './allRoutes.js';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../build')));
 app.use(requestIp.mw());
-
-
-
-
+app.use(cookieParser());
 
 // Add all the routes to our Express server
-// exported from routes/index.js
-routes.forEach(route => {
-    app[route.method](route.path, route.handler);
+routes.forEach((route) => {
+  app[route.method](route.path, route.handler);
 });
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
 export const startServer = () => {
+  app.listen(port, () => {
+    const networkInterfaces = os.networkInterfaces();
+    let ipAddress;
 
-    app.listen(port, () => {
-        const networkInterfaces = os.networkInterfaces();
-        let ipAddress;
-
-        for (const interfaceName in networkInterfaces) {
-            const interfaces = networkInterfaces[interfaceName];
-            for (const iface of interfaces) {
-                if (!iface.internal && iface.family === 'IPv4') {
-                    ipAddress = iface.address;
-                    break;
-                }
-            }
-            if (ipAddress) {
-                break;
-            }
+    for (const interfaceName in networkInterfaces) {
+      const interfaces = networkInterfaces[interfaceName];
+      for (const iface of interfaces) {
+        if (!iface.internal && iface.family === 'IPv4') {
+          ipAddress = iface.address;
+          break;
         }
+      }
+      if (ipAddress) {
+        break;
+      }
+    }
 
-        console.log(`Server is running on port http://localhost:${port}`);
-        if (ipAddress) {
-            console.log(`Locally connected to:  http://${ipAddress}:${port}`);
-        } else {
-            console.log('Unable to determine server IP address.');
-        }
-    });
-}
-
-
-
-
+    console.log(`Server is running on port http://localhost:${port}`);
+    if (ipAddress) {
+      console.log(`Locally connected to:  http://${ipAddress}:${port}`);
+    } else {
+      console.log('Unable to determine server IP address.');
+    }
+  });
+};
 
 app.get('/', (req, res) => {
-
-let ipAddress=  ip.address()
-ip.subnet('192.168.1.134', '255.255.255.192')
-        res.send(`Your IP address is: ${ipAddress}`);
-
-
+  const ipAddress = ip.address();
+  ip.subnet('192.168.1.134', '255.255.255.192');
+  res.send(`Your IP address is: ${ipAddress}`);
 });
-
 
 // Middleware to retrieve the IP address
 app.use(requestIp.mw());
@@ -80,19 +68,27 @@ app.use(requestIp.mw());
 // Register the device middleware
 app.use(device.capture());
 
+const userLocationCache = new Map();
+const responseCache = {};
 
-
-
-app.get('/api/v1',async (req, res) => {
+app.get('/api/v1', async (req, res) => {
   try {
     const ip = getClientIP(req);
     const userAgent = req.headers['user-agent'];
 
+    // Check if the stored IP matches the request IP
+    const storedIP = req.cookies.ipAddress;
+    if (storedIP === ip) {
+      const cachedResponse = responseCache[ip];
+      if (cachedResponse) {
+        res.send(cachedResponse);
+        return; // Return early to skip the remaining code
+      }
+    }
+
     const parsedUserAgent = useragent.parse(userAgent);
     const browser = parsedUserAgent.family;
     const os = parsedUserAgent.os.toString();
-  
-   
 
     const isMobile = detectMobileDevice(req);
     const networkType = isMobile ? 'Mobile Connection' : getNetworkType(req);
@@ -101,26 +97,53 @@ app.get('/api/v1',async (req, res) => {
     const isPostman = detectPostman(req);
     const browserInfo = isPostman ? 'Postman' : browser;
     const isVpn = detectVPN(ip);
-    const Description=   await userLocation(ip);
-    const hostname = getHostName(req);
-   
-const isMobileVpn = MobileVpnDetection(req);
-    res.send({
+
+    // Check if user location is cached
+    let description = userLocationCache.get(ip);
+    if (!description) {
+      description = await userLocation(ip, res);
+      userLocationCache.set(ip, description);
+    }
+
+    const responseStartTime = new Date().getTime(); // Record the response start time
+
+    const responseData = {
       ip,
       networkType,
       deviceType,
       browser: browserInfo,
       os,
       isVpn,
-     
-      isMobileVpn,
-       Description
-      
-    });
+      description,
+      responseTime: '', // Initialize responseTime
+    };
+
+    // Store the IP and response data in cookies
+    res.cookie('ipAddress', ip, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
+    res.cookie('responseData', responseData, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
+
+    // Cache the response data
+    responseCache[ip] = responseData;
+
+    res.send(responseData);
+    
+
+    const responseEndTime = new Date().getTime(); // Record the response end time
+    const responseTime = responseEndTime - responseStartTime; // Calculate the response time in milliseconds
+    responseData.responseTime = `${responseTime}ms`; // Format the response time with 'ms'
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
+
+
+
+
+
+
+
+
+
 
 const getClientIP = (req) => {
   const forwardedFor = req.headers['x-forwarded-for'];
@@ -159,20 +182,15 @@ const isLANConnection = (req) => {
 };
 
 const isPrivateIP = (ip) => {
-  const privateIPRanges = [
-    '10.0.0.0/8',
-    '172.16.0.0/12',
-    '192.168.0.0/16',
-    'fd00::/8',
-  ];
+  const privateIPRanges = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16', 'fd00::/8'];
 
-  return privateIPRanges.some(range => {
+  return privateIPRanges.some((range) => {
     const [subnet, prefix] = range.split('/');
     const subnetParts = subnet.split('.');
     const ipParts = ip.split('.');
 
-    const subnetBinary = subnetParts.map(part => (+part).toString(2).padStart(8, '0')).join('');
-    const ipBinary = ipParts.map(part => (+part).toString(2).padStart(8, '0')).join('');
+    const subnetBinary = subnetParts.map((part) => (+part).toString(2).padStart(8, '0')).join('');
+    const ipBinary = ipParts.map((part) => (+part).toString(2).padStart(8, '0')).join('');
 
     return ipBinary.slice(0, prefix) === subnetBinary.slice(0, prefix);
   });
@@ -185,94 +203,81 @@ const detectVPN = (ip) => {
   if (geo && geo.isp && geo.isp.toLowerCase().includes('vpn')) {
     return true;
   }
-  
+
   // Add additional checks or conditions based on your VPN detection requirements
   const vpnProviders = ['expressvpn', 'nordvpn', 'cyberghost', 'surfshark'];
-  const matchedProvider = vpnProviders.find(provider => geo && geo.isp && geo.isp.toLowerCase().includes(provider));
+  const matchedProvider = vpnProviders.find((provider) => geo && geo.isp && geo.isp.toLowerCase().includes(provider));
   if (matchedProvider) {
     return true;
   }
-  
+
   // TODO: Add additional checks or conditions based on your VPN detection requirements
   const vpnRegions = ['Hong Kong', 'British Virgin Islands', 'Panama', 'Cyprus'];
-  const matchedRegion = vpnRegions.find(region => geo && geo.country && geo.country.toLowerCase().includes(region.toLowerCase()));
+  const matchedRegion = vpnRegions.find(
+    (region) => geo && geo.country && geo.country.toLowerCase().includes(region.toLowerCase())
+  );
   if (matchedRegion) {
     return true;
   }
 
-
   return false;
 };
+// Define async function userLocation with parameters ip and res
+const userLocation = async (ip, res) => {
+  // Check if userLocationCache has ip key
+  if (userLocationCache.has(ip)) {
+    // Return value associated with ip key
+    return userLocationCache.get(ip);
+  }
 
-
-
-const userLocation = async (ip) => {
+  // Define endpoint using ip-api.com and ip parameter
   const endpoint = `http://ip-api.com/json/${ip}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,proxy,hosting,query`;
 
-  const response = await fetch(endpoint);
-  const data = await response.json();
+  try {
+    // Send GET request to endpoint using axios library
+    const { data } = await axios.get(endpoint);
 
-  const { status, message, continent, continentCode, country, countryCode, region, regionName, city, district, zip, lat, lon, timezone, offset, currency, isp, org, as, asname, reverse,proxy, hosting, query } = data;
+    // Remove query key from data object and create new object with remaining properties
+    const locationData = Object.fromEntries(Object.entries(data).filter(([key]) => key !== 'query'));
 
-  return {
-    status,
-    message,
-    continent,
-    continentCode,
-    country,
-    countryCode,
-    region,
-    regionName,
-    city,
-    district,
-    zip,
-    lat,
-    lon,
-    timezone,
-    offset,
-    currency,
-    isp,
-    org,
-    as,
-    asname,
-    reverse,
-  
-    proxy,
-    //hosting,
-   
-  };
-}
+    // Set cookie options
+    const cookieOptions = {
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true
+    };
 
-const getHostName = async (req) => {
-  const hostname = await req.headers.host;
-  return hostname;
-}
+    // Set cookie with name userLocation, value locationData, and options cookieOptions
+    res.cookie('userLocation', locationData, cookieOptions);
 
+    // Set ip key in userLocationCache with value locationData
+    userLocationCache.set(ip, locationData);
 
-const MobileVpnDetection = (req) => {
-  const userAgent = req.headers['user-agent'];
-  const xForwardedFor = req.headers['x-forwarded-for'];
-  const xRealIP = req.headers['x-real-ip'];
-
-  if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
-    // iOS device detected, check VPN status
-    if (!xRealIP || xRealIP.startsWith('17.')) {
-      return true; // VPN detected
-    }
-  } else if (userAgent.includes('Android')) {
-    // Android device detected, check VPN status
-    if (xForwardedFor && xForwardedFor.split(',').length > 1) {
-      return true; // VPN detected
-    }
-  } else if (userAgent.includes('Macintosh') || userAgent.includes('Linux')) {
-    // macOS or Linux device detected, check VPN status
-    if (xForwardedFor && xForwardedFor.split(',').length === 1) {
-      return true; // VPN detected
-    }
+    // Return locationData object
+    return locationData;
+  } catch (error) {
+    // Send error message with status 500
+    res.status(500).send({
+      status: false,
+      message: error.message
+    });
   }
-  
-  return false; // VPN not detected
 };
+// To retrieve the user location data from the cookie:
 
+app.get('/cookies', (req, res) => {
+  //TODO : if response has empty   send response as no cookies present
 
+  const userLocation = req.cookies.userLocation;
+  res.json(userLocation);
+});
 
+app.get('/api/cookies', (req, res) => {
+  const cookies = req.cookies;
+  const responseData = cookies.responseData;
+
+  if (responseData) {
+    res.send(responseData);
+  } else {
+    res.status(404).send('No cookies data found.');
+  }
+});
